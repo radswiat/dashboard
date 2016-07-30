1f125569f4068a88ba2679473419b1b4541302eb
Natthaniell <rad.swiat@gmail.com>
2016-07-30 03:11:29 +0100

    Jenkins pre-setup

--- src/client/app/app.tsx
+++ src/client/app/app.tsx
@@ -1,13 +1,18 @@
 require('../index.html');
 import * as React       from 'react';
 import * as ReactDOM    from 'react-dom';
-
+import {Auth} from './components/auth/auth.component';
 
 interface HelloProps { compiler: string; framework: string; }
 
 class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return(
+            <div>
+                <h1>Real Time Dashboard</h1>
+                <Auth />
+            </div>
+        );
     }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,100 +1,108 @@
-//// REQUIRE
-//var webpack = require('webpack');
-//var path = require('path');
-//var HtmlWebpackPlugin = require('html-webpack-plugin');
-//
-//// PRE-CONFIG
-//var BUILD_DIR = path.resolve(__dirname, 'dev/client');
-//var APP_DIR = path.resolve(__dirname, 'src/client');
-//
-//var config = {
-//
-//    entry: APP_DIR + '/app/app.jsx',
-//    output: {
-//        path: BUILD_DIR,
-//        filename: 'bundle.js'
-//    },
-//
-//
-//    module : {
-//        loaders : [
-//
-//            {
-//                test : /\.jsx?/,
-//                include : APP_DIR,
-//                loader : 'babel'
-//            },
-//            {
-//                test: /\.html$/,
-//                loader: "raw-loader"
-//            }
-//        ]
-//    },
-//    plugins: [
-//        new HtmlWebpackPlugin()
-//    ]
-//};
-//
-//module.exports = config;
-
-
-
-
-
-
 // REQUIRE
 var webpack = require('webpack');
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var nodeExternals = require('webpack-node-externals');
 
 // PRE-CONFIG
-var BUILD_DIR = path.resolve(__dirname, 'dev/client');
+var BUILD_DIR = path.resolve(__dirname, 'dev');
 var APP_DIR = path.resolve(__dirname, 'src/client');
+var SERVER_DIR = path.resolve(__dirname, 'src/server');
 
-var config = {
+var config = [
+//////////////////////////////////////////////
+// WEBPACK CLIENT BUILD
+//////////////////////////////////////////////
+    {
 
-    entry: APP_DIR + '/app/app.tsx',
-    output: {
-        path: BUILD_DIR,
-        filename: 'bundle.js'
-    },
-
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".jsx", ".js", ".html"]
-    },
-
-    module : {
-        loaders : [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader"
-            },
-            //{
-            //    test : /\.jsx?/,
-            //    include : APP_DIR,
-            //    loader : 'babel'
-            //},
-            {
-                test: /\.html$/,
-                loader: "raw-loader"
-            }
+        entry: [
+            APP_DIR + '/app/app.tsx'
         ],
-        preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.jsx$/, loader: "source-map-loader" }
+        output: {
+            path: BUILD_DIR + '/public',
+            filename: 'bundle.js'
+        },
+
+        // Enable sourcemaps for debugging webpack's output.
+        devtool: "source-map",
+
+        resolve: {
+            // Add '.ts' and '.tsx' as resolvable extensions.
+            extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".jsx", ".js", ".html"]
+        },
+
+        module: {
+            loaders: [
+                {
+                    test: /\.tsx?$/,
+                    loader: "ts-loader"
+                },
+                //{
+                //    test : /\.jsx?/,
+                //    include : APP_DIR,
+                //    loader : 'babel'
+                //},
+                {
+                    test: /\.html$/,
+                    loader: "raw-loader"
+                }
+            ],
+            preLoaders: [
+                // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+                {test: /\.jsx$/, loader: "source-map-loader"}
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'RealTimeDashboard',
+                filename: 'index.html',
+                template: APP_DIR + '/index.html'
+            })
         ]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title : 'RealTimeDashboard',
-            filename: 'index.html',
-            template : APP_DIR + '/index.html'
-        })
-    ]
-};
+
+    //////////////////////////////////////////////
+    // WEBPACK SERVER BUILD
+    //////////////////////////////////////////////
+    {
+        //entry:  SERVER_DIR + '/server.js',
+        entry: [
+            SERVER_DIR + '/server.js'
+        ],
+        target: 'node',
+        node: {
+            __dirname: false,
+            __filename: false,
+        },
+        output: {
+            path: BUILD_DIR,
+            filename: 'server.js'
+        },
+
+        resolve: {
+            // Add '.ts' and '.tsx' as resolvable extensions.
+            extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".jsx", ".js", ".html"]
+        },
+
+        module: {
+            loaders: [
+                {
+                    test : /\.js?/,
+                    include : SERVER_DIR,
+                    loader : 'babel'
+                },
+                //{
+                //    test : /\.js?/,
+                //    include : SERVER_DIR,
+                //    loader : 'node'
+                //},
+                {
+                    test: /\.html$/,
+                    loader: "raw-loader"
+                }
+            ]
+        },
+        externals: [nodeExternals()]
+    }];
 
 module.exports = config;
